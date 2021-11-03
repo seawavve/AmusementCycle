3445df7c32d31af929b1244570cf89306a7d7751
조한희 <66352658+seawavve@users.noreply.github.com>
2021-11-03 11:35:25 +0900

    Update video_to_image.py

--- autoencoder/video_to_image.py
+++ autoencoder/video_to_image.py
@@ -2,16 +2,16 @@ import numpy as np
 import cv2
 import os
 import json
-import imutils
-VIDEOS_DIR=r"/Users/seawavve/Desktop/swing_PBL/swingmybaby1 2/"
-FILE_NAME=r"gyro_swings1"
+VIDEOS_DIR=r"C:/Users/조한희/Desktop/gyro_swing1_imgs/"
+FILE_NAME=r"gyro_swing1"
 
 
 def get_preprocessed_video():
     print("--------Preprocess Train Video-------")
     print('input file name: ',FILE_NAME)
     input_path=os.path.join(VIDEOS_DIR, FILE_NAME + '.mp4')
- 
+    print(input_path)
+    
     cap = cv2.VideoCapture(input_path)
     # Initialize frame counter
     cnt = 0
@@ -26,9 +26,12 @@ def get_preprocessed_video():
         ret, frame = cap.read()
         if ret==True:
             cnt+=1
-            print('NOW output FRAME:', cnt)
-            frame = imutils.resize(frame, width=500)
-            cv2.imwrite("/Users/seawavve/Desktop/swing_PBL/img/"+FILE_NAME+"/frame%d.jpg" % cnt, frame)
+            if cnt%2==0:continue
+            # print('NOW output FRAME:', cnt)
+            frame =cv2.resize(frame,dsize=(100,100))
+            #print(frame.shape)
+            print("./data/test/frame%d.jpg" % cnt)
+            cv2.imwrite("./data/test/frame%d.jpg" % cnt, frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
